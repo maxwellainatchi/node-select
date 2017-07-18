f9850b793b64b6834584aeaeff2de37cbabbea53
Max Ainatchi <maxwell.ainatchi@gmail.com>
2017-07-18 11:55:39 -0400

    Updated to allow default cases as well as actions.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = function(value, cases) {
     let result;
     if (cases[value]) {
         result = cases[value];
+    } else if (cases.default_case && cases[cases.default_case]) {
+        result = cases[cases.default_case];
     } else if (cases.default) {
         result = cases.default;
     }
